2257841c9356d410641ba61863fa1c401fb02942
Ghaida Saeed <ghaidaa.s17@gmail.com>
2021-02-09 02:27:19 +0300

    Fix javascript issue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'packs/jquery.min',
+    jQuery: 'packs/jquery.min'
+  })
+)
+
 module.exports = environment
