049592268a5e55e18a4280a77536fd8313139e65
Ghaida Saeed <ghaidaa.s17@gmail.com>
2021-04-07 13:45:22 +0300

    Add attacks table & timeline chart to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class DashboardController < ApplicationController
-
-
+  before_action :authenticate_user!
   def index
     @uploads = current_user.uploads
+    @uploads = @uploads.where(id: params[:upload_id]) if params[:upload_id]
     # Get statistics data
     @statistics = Statistic.where(upload_id: @uploads.ids)
     @total_traffic = @statistics.sum(&:total_records)
@@ -19,12 +19,34 @@ class DashboardController < ApplicationController
   end
 
   def attacks_per_day
-    @attacks = Attack.joins(upload: :user).where('users.id = ?', current_user.id)
+    # NOT IMPORTANT:
+    # NOTE: every group_by & map is actually looping through the attacks
+    # we can enhancee the performance here by looping through the attacks for ONE time, and add the data to a hash map
+    # so it would bee something like:
+    # results = { 'ddos' => [], 'dos' => [], 'theft' => [], 'reconnaissance' => [] }
+    # @attacks.each |att| do
+    #   sdate = att.started_at.strftime("%Y-%m-%d")
+    #   arr = results[att.attack_type]
+      # day_attacks = arr.find { |d| d[:x] == sdate }
+      # if day_attacks
+      #   day_attacks[:y] += 1
+      # else
+      #   arr.push({x: sdate, y: 1})
+      # end
+    # end
+    start_date = params['start_date']&.to_datetime
+    end_date = params['end_date']&.to_datetime
+
+    # Main query
+    @attacks = Attack.joins(upload: :user).order(:started_at).where('users.id = ?', current_user.id)
+    @attacks = @attacks.where(started_at: start_date..end_date) if start_date && end_date
+    @attacks = @attacks.where(upload_id: params[:upload_id]) if params[:upload_id]
+
     attacks_per_type = @attacks.group_by { |a| a.attack_type }
-    @ddos = attacks_per_type['ddos'].group_by{ |a| a.started_at.strftime("%Y-%m-%d") }.map { |k, v| { x: k, y: v.count } }
-    @dos = attacks_per_type['dos'].group_by{ |a| a.started_at.strftime("%Y-%m-%d") }.map { |k, v| { x: k, y: v.count } }
-    @theft = attacks_per_type['theft'].group_by{ |a| a.started_at.strftime("%Y-%m-%d") }.map { |k, v| { x: k, y: v.count } }
-    @reconnaissance = attacks_per_type['reconnaissance'].group_by{ |a| a.started_at.strftime("%Y-%m-%d") }.map { |k, v| { x: k, y: v.count } }
+    @ddos = attacks_per_type['ddos']&.group_by{ |a| a.started_at.strftime("%Y-%m-%d") }&.map { |k, v| { x: k, y: v.count } }
+    @dos = attacks_per_type['dos']&.group_by{ |a| a.started_at.strftime("%Y-%m-%d") }&.map { |k, v| { x: k, y: v.count } }
+    @theft = attacks_per_type['theft']&.group_by{ |a| a.started_at.strftime("%Y-%m-%d") }&.map { |k, v| { x: k, y: v.count } }
+    @reconnaissance = attacks_per_type['reconnaissance']&.group_by{ |a| a.started_at.strftime("%Y-%m-%d") }&.map { |k, v| { x: k, y: v.count } }
     render json: { ddos: @ddos, dos: @dos, theft: @theft, reconnaissance: @reconnaissance }, status: :ok
   end
 end
