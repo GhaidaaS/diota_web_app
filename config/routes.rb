cc5eef5bc08cf239f3d76ec963ce541283910b66
Ghaida Saeed <ghaidaa.s17@gmail.com>
2021-04-07 13:35:47 +0300

    Add user profile

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,15 @@ Rails.application.routes.draw do
   get "/dashboard" => "dashboard#index"
   get "/dashboard/attacks_per_day" => "dashboard#attacks_per_day"
   resources :uploads, only: [:index, :create]
-  get "/add_users" => "users#index"
-  post '/add_users' => 'users#create'
+  get "/manage_users" => "users#index"
+
+  get '/create_users' => 'users#add_user'
+  post '/create_users' => 'users#create'
+
+  get '/user_profile' => 'users#profile'
+  patch '/update_profile' => 'users#update_profile'
+  delete '/delete_user' => 'users#delete_user'
+
   get "/attacks" => "attacks#index"
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
