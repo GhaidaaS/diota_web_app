3019b338b00c00e7470db03a01cb3d2cad536945
Ghaida Saeed <ghaidaa.s17@gmail.com>
2021-04-07 13:46:14 +0300

    Add attacks index with filters

--- app/controllers/attacks_controller.rb
+++ app/controllers/attacks_controller.rb
@@ -1,7 +1,16 @@
 class AttacksController < ApplicationController
   before_action :authenticate_user!
+
+  # Scopes
+  has_scope :by_attack_type
+  has_scope :by_source_port
+  has_scope :by_destination_port
+  has_scope :by_source_ip
+  has_scope :by_destination_ip
+
   def index
     @uploads = current_user.uploads
-    @attacks = Attack.where(upload_id: @uploads.ids)
+    # @attacks = Attack.where(upload_id: @uploads.ids)
+    @attacks = apply_scopes(Attack).where(upload_id: @uploads.ids)
   end
 end
--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,5 +1,11 @@
 class Attack < ApplicationRecord
   belongs_to :upload
+
+  scope :by_attack_type, -> attack_type { where(attack_type: attack_type) }
+  scope :by_source_port, -> source_port { where(source_port: source_port) }
+  scope :by_destination_port, -> destination_port{ where(destination_port: destination_port) }
+  scope :by_source_ip, -> source_ip{ where(source_ip: source_ip) }
+
   enum attack_type: {
     ddos: 1,
     dos: 2,
