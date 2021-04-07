5af01aee47698c202da5f013d0c0f74eef667d3f
Ghaida Saeed <ghaidaa.s17@gmail.com>
2021-04-07 13:31:27 +0300

    Move uploading csv to separate class

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,30 +1,49 @@
 class UploadsController < ApplicationController
   before_action :authenticate_user!
 
+  # Index page for uploads (scan page)
   def index
     @upload = Upload.new
-    @uploads = Upload.limit(10).order(created_at: :desc)
+    @uploads = Upload.where(user: current_user).limit(10).order(created_at: :desc)
   end
 
+  # Uploads CSV to the deep learning model
   def create
-    # read file
-    uploaded_io = params[:upload][:csv_file]
-    # TODO: Validate that CSV file has correct columns & has at least 1 row
-    # Create upload record
-    upload = Upload.create!(user: current_user, status: :processing)
+    ActiveRecord::Base.transaction do
+      # read the file from the params
+      uploaded_io = params[:upload][:csv_file]
 
-    # TODO: insert all data in temo_upload table
-    # POST JSON content
-    require 'CSV'
-    data = []
-    CSV.foreach(uploaded_io.path, encoding: "UTF-8-BOM", headers: true, header_converters: lambda { |name| Constants.csv_column_names_lookup[name] }) do |row|
-      data << row.to_hash # hash
+      # Create upload record in the DB
+      upload = Upload.create!(user: current_user, status: :processing)
+
+      # Use CSV library to extract the data from the uploaded file
+      require 'CSV'
+      # headers = CSV.read(uploaded_io)
+      data = []
+      CSV.foreach(uploaded_io.path, encoding: "UTF-8-BOM", headers: true, header_converters: lambda { |name| Constants.csv_column_names_lookup[name] }).with_index do |row, i|
+        validate_csv_headers(row.headers) if i.zero?
+        data << row.to_hash # hash
+      end
+
+      # Call the Flask API to get the prediction results
+      DataUploader.call(data, upload)
+    rescue => error
+      flash[:alert] = error.message
+      raise ActiveRecord::Rollback
     end
-    resp = Faraday.post("http://localhost:5000/predict", { data: data }.to_json, "Content-Type" => "application/json")
-
-    # UploadWorker.perform_async(upload.id)
-    # DataUploader.run(upload, file)
 
     redirect_to uploads_path
+  end
+
+  private
+
+  def validate_csv_headers(file_headers)
+    missed_headers = []
+    required_headers = Constants.required_csv_headers
+    required_headers.each do |header|
+      missed_headers.push(header) unless file_headers.include?(header)
+    end
+    return if missed_headers.empty?
+    raise "Missing columns: #{missed_headers.join(', ')}"
   end
 end
--- lib/data_uploader.rb
+++ lib/data_uploader.rb
@@ -1,8 +1,51 @@
 class DataUploader
-  # Call library class
-  # Convert csv to json
-  # Send the json as a rquest to the API
-  # Create statistic record
-  # Bulk insert attacks records
-  # Update upload status & finished_at
+  def self.call(data, upload)
+    resp = Faraday.post("http://localhost:5000/predict", { data: data }.to_json, "Content-Type" => "application/json")
+
+    # fetching the payload & stats/attacks objects from it
+    response_body = JSON.parse(resp.body)
+    statistics = response_body['statistics']
+    attacks = response_body['attacks']
+
+    # creating stats record with the mapped values from the payload
+    Statistic.create!(
+      upload: upload,
+      total_records: statistics['total_records'].to_i,
+      total_attacks: statistics['total_attacks'].to_i,
+      total_ddos: statistics['total_ddos'].to_i,
+      total_dos: statistics['total_dos'].to_i,
+      total_theft: statistics['total_theft'].to_i,
+      total_reconnaissance: statistics['total_reconnaissance'].to_i,
+    )
+
+    # To use bulk insert in one query for all attacks, we need the data in this format: [{..attack_attrs}, {..attack_attrs}]
+    # So here we map the array from the payload to contain array of hashes
+    attacks.map! do |attack|
+      {
+        upload_id: upload.id,
+        source_ip: attack['saddr'],
+        source_port: attack['sport'],
+        destination_ip: attack['daddr'],
+        destination_port: attack['dport'],
+        duration: attack['dur'],
+        sequence_id: attack['pkSeqID'],
+        attack_type: attack['attack_type'].downcase,
+        created_at: Time.now,
+        updated_at: Time.now,
+        started_at: Time.at(attack['stime'].to_i),
+        ended_at: Time.at(attack['ltime'].to_i),
+      }
+    end
+
+    # insert all attacks in a single query
+    Attack.insert_all!(attacks)
+
+    # This updates the upload status to be processed
+    upload.update!(status: :processed, finished_at: Time.now)
+
+  rescue => error
+    # catch any error and update the upload to be failed!
+    upload.update!(status: :failed)
+    raise error.to_s
+  end
 end
